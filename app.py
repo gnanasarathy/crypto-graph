18dfd2c27bf5205eb731d840d3173ee6344f89cd
gnanasarathy <58853340+gnanasarathy@users.noreply.github.com>
2022-05-04 19:53:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, request, redirect
 import json
 from flask_cors import CORS
+from predict import prepareData, reloadJson
 app = Flask(__name__)
 cors = CORS(app)
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
+    reloadJson()
     return render_template('index.html')
 
-
-j = [{1:"hello"}, {2:"hi"}]
-
-js = json.dumps(j)
 @app.route("/file", methods=["GET","POST"])
 def file():
-    return js
+    json_file = open('sample.json')
+    return json.load(json_file)
 
 
 if __name__ == "__main__":
